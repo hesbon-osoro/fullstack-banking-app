a893ee9234101d78161a0bfc0c6754b84e36c2ae
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-13 12:30:41 +0300

    app root file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
+import AppRouter from './router/AppRouter';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/main.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<Provider store={store}>
+		<AppRouter />
+	</Provider>
 );
