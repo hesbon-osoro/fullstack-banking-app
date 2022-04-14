f6e82c97ae02541f57b13084aaa69168f5b0b46a
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-14 18:34:29 +0300

    store update

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,14 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import authReducer from '../reducers/auth';
-
+import errorsReducer from '../reducers/errors';
+import profileReducer from '../reducers/profile';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
 	combineReducers({
 		auth: authReducer,
+		errors: errorsReducer,
+		profile: profileReducer,
 	}),
 	composeEnhancers(applyMiddleware(thunk))
 );
