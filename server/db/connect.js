ad8add230b0de47f0d02ecdeac3740d8834db3a7
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-14 23:22:54 +0300

    hiding app credentials

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-	user: 'postgres',
-	password: 'root',
-	host: 'localhost',
-	port: 5432,
-	database: 'bank_account',
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	host: process.env.POSTGRES_HOST,
+	port: process.env.POSTGRES_PORT,
+	database: process.env.POSTGRES_DATABASE,
 });
 
 module.exports = { pool };
