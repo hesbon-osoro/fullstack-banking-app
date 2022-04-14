2413d2de7086b33d89f7649a6d400ded4b615082
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-14 18:35:00 +0300

    App router update

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch, Router } from 'react-router-dom';
 import Login from '../components/Login';
 import Register from '../components/Register';
+import Profile from '../components/Profile';
+import { createBrowserHistory } from 'history';
+import Header from '../components/Header';
+import Logout from '../components/Logout';
 
-const AppRouter = () => {
+export const history = createBrowserHistory();
+
+const AppRouter = ({ auth }) => {
 	return (
-		<Router>
+		<Router history={history}>
 			<div className="container">
+				{!_.isEmpty(auth.token) && <Header />}
 				<Switch>
 					<Route path="/" component={Login} exact={true} />
 					<Route path="/register" component={Register} />
+					<Route path="/profile" component={Profile} />
+					<Route path="/logout" component={Logout} />
 				</Switch>
 			</div>
 		</Router>
