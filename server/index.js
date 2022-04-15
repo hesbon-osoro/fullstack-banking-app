834f6a68f75a8ab054396e3ddb9976c30b7dab60
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-16 00:35:14 +0300

    account route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const authRoute = require('./routes/auth');
 const profileRoute = require('./routes/profile');
+const accountRoute = require('./routes/account');
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(authRoute);
+app.use(accountRoute.Router);
 app.use(profileRoute);
 app.use('*', (_req, res) => res.json('Page not found.'));
 
