d90a4e63cab7d7f591238f592c34b00491596a17
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-15 12:01:14 +0300

    hashed password length

--- server/scripts.sql
+++ server/scripts.sql
@@ -7,13 +7,13 @@ CREATE TABLE bank_user(
     first_name VARCHAR(32) NOT NULL,
     last_name VARCHAR(32) NOT NULL,
     email VARCHAR(32) NOT NULL,
-    password VARCHAR(32) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     unique(email)
-)
+);
 
 CREATE TABLE TOKENS(
     id BIGSERIAL PRIMARY KEY NOT NULL,
     access_token VARCHAR(500) NOT NULL,
     userid BIGSERIAL NOT NULL,
     FOREIGN KEY(userid) REFERENCES bank_user(userid)
-)
+);
