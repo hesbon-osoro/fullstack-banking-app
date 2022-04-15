e3b1a1a736baa14a318b0137a2b0f957aac3b18f
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-16 02:19:01 +0300

    other styling libraries

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import AppRouter from './router/AppRouter';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-datepicker/dist/react-datepicker.min.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import './css/main.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
