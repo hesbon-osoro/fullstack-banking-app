6506c9d93f884f8917827f3fa6fb32f2a269f190
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-16 12:55:12 +0300

    removing api url since it is provided as proxy in package.json

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
-export const BASE_API_URL = 'http://localhost:5000';
+export const BASE_API_URL = '';
 export const GET_ERRORS = 'GET_ERRORS';
 export const RESET_ERRORS = 'RESET_ERRORS';
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
