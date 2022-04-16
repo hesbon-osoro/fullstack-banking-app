8348ba1bcc78e0cb7d9137264fa321ba336e920c
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-16 23:28:14 +0300

    maintaining session refactored

--- src/utils/common.js
+++ src/utils/common.js
@@ -10,17 +10,10 @@ export const validateFields = fieldsToValidate => {
 };
 
 export const maintainSession = () => {
-	const user_token = localStorage.getItem('user_token');
+	const currentPath = window.location.pathname;
 
-	if (user_token) {
-		const currentPath = window.location.pathname;
-		if (currentPath === '/' || currentPath === '/register') {
-			history.push('/profile');
-		}
-		const decoded = jwt_decode(user_token);
-		updateStore(decoded);
-	} else {
-		history.push('/');
+	if (currentPath === '/profile') {
+		store.dispatch(initiateGetProfile());
 	}
 };
 
