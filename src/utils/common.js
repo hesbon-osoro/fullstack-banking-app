8eb62d11b7a5f221e8189b4639fc4291520443d8
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-16 23:30:19 +0300

    removing unneccessary code

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,7 +1,5 @@
 import store from '../store/store';
 import { initiateGetProfile } from '../actions/profile';
-import { signIn } from '../actions/auth';
-import Axios from 'axios';
 
 export const validateFields = fieldsToValidate => {
 	return fieldsToValidate.every(field => Object.values(field)[0] !== '');
@@ -13,27 +11,4 @@ export const maintainSession = () => {
 	if (currentPath === '/profile') {
 		store.dispatch(initiateGetProfile());
 	}
-};
-
-export const updateStore = user => {
-	const { userid, email } = user;
-	store.dispatch(
-		signIn({
-			userid,
-			email,
-			token: localStorage.getItem('user_token'),
-		})
-	);
-	store.dispatch(initiateGetProfile(email));
-};
-
-export const setAuthHeader = () => {
-	const token = localStorage.getItem('user_token');
-	if (token) {
-		Axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-	}
-};
-
-export const removeAuthHeader = () => {
-	delete Axios.defaults.headers.common['Authorization'];
 };
