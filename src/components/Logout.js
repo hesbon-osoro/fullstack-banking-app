3e16507ebfccd75f86450eed0dcebb41af2afb34
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-20 17:29:43 +0300

    Logout.js

---
+++ src/components/Logout.js
@@ -0,0 +1,22 @@
+import { useEffect, useCallback, useRef } from 'react';
+import { connect } from 'react-redux';
+import { initiateLogout } from '../actions/auth';
+
+const Logout = props => {
+	const propsRef = useRef(false);
+	const processOnMount = useCallback(() => {
+		if (propsRef.current === false) {
+			const { history, dispatch } = props;
+			dispatch(initiateLogout()).then(() => history.push('/'));
+			propsRef.current = true;
+		}
+	}, [props]);
+
+	useEffect(() => {
+		processOnMount();
+	}, [processOnMount]);
+	
+	return null;
+};
+
+export default connect(Logout);
